04d5deb84cc0f14b1511e675d179c8450174edac
khajamdajmeer <khajamdajmeer@gmail.com>
2023-08-20 23:58:07 +0530

    added the css for the client page

--- src/ApiCalls/AdminCalls/ProgressCalls.js
+++ src/ApiCalls/AdminCalls/ProgressCalls.js
@@ -1,18 +1,21 @@
+import axios from 'axios';
 import Cookies from 'js-cookie';
-import {host} from '../Host';
+import { host } from '../Host';
 
 const header = {
-    'Content-Type':'application/json',
-    'auth-token':Cookies.get('auth-token')
-}
+    'Content-Type': 'application/json',
+    'auth-token': Cookies.get('auth-token')
+};
 
-export const GetCount=async(id)=>{
-    const response = await fetch(`${host}/api/progress/reqcount/${id}`,{
-        method:'GET',
-        headers:header
-
-    });
-    const res =response.json();
-    return res;
-
-}
+export const GetCount = async (id) => {
+    try {
+        const response = await axios.get(`${host}/api/progress/reqcount/${id}`, {
+            headers: header
+        });
+        return response.data;
+    } catch (error) {
+        // Handle error here
+        console.error('An error occurred:', error);
+        throw error; // Rethrow the error to handle it in the calling code
+    }
+};
--- src/Components/Managment/Admin/Clients/Clients.js
+++ src/Components/Managment/Admin/Clients/Clients.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Clients.css';
 import serachicon from '../../../images and tones/search-icon.png'
+import img3dots from '../../../images and tones/3dot.png'
 
 const Clients = () => {
+
+  //logic for showing downloadbtns
+  const [showDwnlod,setShowDwnlod]=useState(false)
+  const handleShowDwnload =()=>{
+    setShowDwnlod(!showDwnlod)
+  }
+
+
   return (
     <>
     <div className="ad-c-fullscreen">
@@ -10,6 +19,7 @@ const Clients = () => {
         <div className="ad-c-head">
           <div className="ad-c-hleft">Clients</div>
           <div className="ad-c-hright">
+            <div className="ad-c-serachdiv">
             <select name="type" id="" className='ad-c-selectip'>
               <option value="0">---select---</option>
               <option value="name">Name</option>
@@ -18,8 +28,18 @@ const Clients = () => {
             <input type="text" className="ad-c-searchip" placeholder='Search'/>
             <button className="ad-c-searchbtn">
               <img src={serachicon} alt="" />
+            </button></div>
+            <div className='ad-c-3dot-hide'>
+            <button className="ad-c-3dot" onClick={handleShowDwnload}>
+              <img src={img3dots} alt="" />
             </button>
+            {showDwnlod&&(<div className="ad-c-3dot-hbody">
+              <button>download</button>
+              <button>download all</button>
+            </div>)}
+            </div>
           </div>
+
         </div>
         <div className="ad-c-body">
         <div className="ad-c-mapitem">
@@ -28,6 +48,7 @@ const Clients = () => {
           <div className="ad-c-names"> malakpet</div>
           <div className="ad-c-names">servcie type acservice here</div>
           <div className="ad-c-names"> discription is discriptiondiscriptiondiscription</div>
+          <div className="ad-c-names"> <button className='ad-c-viewbtn'>view</button></div>
         </div>
         </div>
 
@@ -41,6 +62,7 @@ const Clients = () => {
           
           <li className='ad-c-ul-prev' >Next</li>
         </ul>
+        
       </div>
       </div>
     </div>
